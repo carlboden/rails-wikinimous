344e12695177ab23e3be8f52b5e990d73c1d6416
Carl Boden <carl_boden@hotmail.com>
2020-05-12 16:31:40 +0200

    sol

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  article = Article.new(
+    title = Faker::Appliance.equipment,
+    content = Faker::Lorem.sentence
+  )
+  article.save!
+end
